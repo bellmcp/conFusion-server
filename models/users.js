2246e2a628af626fe527efc093bbbed0ed39dcc0
Wutipat Khamnuansin <bellmcp@gmail.com>
2020-09-19 21:06:43 +0700

    mongoose population

--- models/users.js
+++ models/users.js
@@ -2,13 +2,26 @@ var mongoose = require("mongoose");
 var Schema = mongoose.Schema;
 var passportLocalMongoose = require("passport-local-mongoose");
 
-var User = new Schema({
+var userSchema = new Schema({
+  firstname: {
+    type: String,
+    default: "",
+  },
+  lastname: {
+    type: String,
+    default: "",
+  },
   admin: {
     type: Boolean,
     default: false,
   },
 });
 
-User.plugin(passportLocalMongoose);
+userSchema.plugin(passportLocalMongoose);
 
-module.exports = mongoose.model("user", User);
+// module.exports = mongoose.model("user", User);
+var Users = mongoose.model("User", userSchema);
+module.exports = Users;
+
+// var Promotions = mongoose.model("Promotion", promotionSchema);
+// module.exports = Promotions;
